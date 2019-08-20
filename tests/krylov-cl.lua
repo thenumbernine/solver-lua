5647490878a45c705622c297081c26f76d5cae61
Christopher Moore <christopher.e.moore@gmail.com>
2019-08-20 17:57:59 +0800

    fixed the krylov-cl test

--- tests/krylov-cl.lua
+++ tests/krylov-cl.lua
@@ -1,12 +1,18 @@
 #!/usr/bin/env luajit
 require 'ext'
-local cl = require 'cl'
+local cl = require 'ffi.OpenCL'
 local matrix = require 'matrix'
 local gnuplot = require 'gnuplot'
 
 local n = 128
 
-local env = require 'cl.obj.env'{size={n,n}, precision='float'}
+local env = require 'cl.obj.env'{
+	size = {n, n},
+	precision = 'float',
+	queue = {
+		properties = cl.CL_QUEUE_PROFILING_ENABLE,
+	},
+}
 
 -- A x = b ... solve for x
 local b = env:buffer{name='b'}
@@ -21,6 +27,7 @@ env:kernel{
 ]],
 }()
 
+local event = require 'cl.event'()
 local A = env:kernel{
 	argsOut = {{name='y', type='global real*', buffer=true}},
 	argsIn = {{name='x', type='const global real*', buffer=true}},
@@ -48,6 +55,7 @@ local A = env:kernel{
 		- 4. * x[index]
 	) / hSq;
 ]],
+	event = event,
 }
 
 local function splot(gpubuf, name)
@@ -71,19 +79,25 @@ local x = env:buffer{name='x'}
 
 for _,solver in ipairs{
 	--'conjgrad',
-	--'conjres',
+	'conjres',
 	--'bicgstab',
-	'gmres',
+	--'gmres',
 } do
 	require('solver.cl.'..solver){
 		env = env,
 		A = A,
 		b = b,
 		x = x,
-		errorCallback = function(err, iter, x)
-			print(err, iter)
+		errorCallback = function(res, iter, x)
+			print(iter, res)
 		end,
 		restart = 10,
+		maxiter = 1000,
 	}()
 	splot(x, 'x-'..solver)
 end
+
+env.cmds[1]:finish()
+local start = event:getProfilingInfo'CL_PROFILING_COMMAND_START'
+local fin = event:getProfilingInfo'CL_PROFILING_COMMAND_END'
+print('duration', tonumber(fin - start)..' ns')
