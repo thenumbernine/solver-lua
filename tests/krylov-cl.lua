b48893755f7e814242034cc63582049409ec63c3
Christopher Moore <christopher.e.moore@gmail.com>
2019-08-22 18:18:19 +0800

    changing krylov-cl to work with multiple gpus

--- tests/krylov-cl.lua
+++ tests/krylov-cl.lua
@@ -3,6 +3,7 @@ require 'ext'
 local cl = require 'ffi.OpenCL'
 local matrix = require 'matrix'
 local gnuplot = require 'gnuplot'
+local ffi = require 'ffi'
 
 local n = 128
 
@@ -14,49 +15,219 @@ local env = require 'cl.obj.env'{
 	},
 }
 
+local regionType = [[
+typedef struct {
+	int4 supersize;	//size of the parent buffer
+	int4 size;		//size of this buffer
+	int4 step;		//step of this buffer (based on subbuffer / xreadmax-xreadmin)
+	
+	//(n-1)-axis chopped up
+	int readmin;	//sub-buffer range
+	int readmax;
+	int writemin;	//kernel range
+	int writemax;
+} region_t;
+]]
+ffi.cdef(regionType)
+
+
+local domains = table()
+local regions = table()		-- CPU
+local regionBufs = table()	-- GPU
+
+for i,device in ipairs(env.devices) do
+	local nymin = math.floor(n*(i-1)/#env.devices)
+	local nymax = math.floor(n*i/#env.devices)
+
+	local region = ffi.new'region_t[1]'
+	region[0].supersize.x = n
+	region[0].supersize.y = n
+	region[0].supersize.z = 1
+
+	region[0].writemin = nymin
+	region[0].writemax = nymax
+
+	if i > 1 then nymin = nymin - 1 end
+	if i < #env.devices then nymax = nymax + 1 end
+
+	region[0].readmin = nymin
+	region[0].readmax = nymax
+
+	region[0].size = region[0].supersize
+	region[0].size.y = region[0].readmax - region[0].readmin
+	
+	region[0].step.x = 1
+	region[0].step.y = region[0].size.x
+	region[0].step.z = region[0].size.y * region[0].step.x
+	region[0].step.w = region[0].size.z * region[0].step.y
+
+	domains:insert(require 'cl.obj.domain'{
+		env = env,
+		device = device,
+		size = {region[0].size.x, region[0].size.y},
+	})
+
+	local regionBuf = env:buffer{
+		name = 'region',
+		type = 'region_t',
+		count = 1,
+		readwrite = 'read',
+		constant = true,
+		data = region,
+	}
+	regions:insert(region)
+	regionBufs:insert(regionBuf)
+end
+
 -- A x = b ... solve for x
 local b = env:buffer{name='b'}
 
-env:kernel{
-	argsOut = {b},
-	body = [[
-	b[index] = (
-		i.x >= size.x/4 && i.x < size.x*3/4 &&
-		i.y >= size.y/4 && i.y < size.y*3/4
+-- TODO returning 'x' doesn't return an env:buffer object
+local x = env:buffer{name='x'}
+
+local program = env:program{
+	code = table{
+		regionType,
+		[[
+kernel void bInit(
+	constant region_t* region,
+	global float* b
+) {
+	//cl global_id range / range in write
+	int4 iwr = globalInt4();
+	if (iwr.x >= region->size.x) return;
+	if (iwr.y >= region->writemax - region->writemin) return;
+
+	//range in subbuffer
+	int4 ird = iwr;
+	ird.y += region->readmin - region->writemin;
+	int subindex = indexForInt4ForSize(ird, region->size.x, region->size.y, region->size.z);
+
+	//range in the super buffer
+	int4 i = iwr;
+	i.y += region->writemin;
+	
+	b[subindex] = (
+		i.x >= region->supersize.x/4 && i.x < region->supersize.x*3/4 &&
+		i.y >= region->supersize.y/4 && i.y < region->supersize.y*3/4
 	) ? 1. : 0.;
-]],
-}()
+}
 
-local event = require 'cl.event'()
-local A = env:kernel{
-	argsOut = {{name='y', type='global real*', buffer=true}},
-	argsIn = {{name='x', type='const global real*', buffer=true}},
-	body = [[
-	//OOB
-	if (i.x >= size.x || i.y >= size.y) return;	
+kernel void A(
+	constant region_t* region,
+	global float* y,
+	global const float* x
+) {
+	//cl global_id range / range in write
+	int4 iwr = globalInt4();
+	if (iwr.x >= region->size.x) return;
+	if (iwr.y >= region->writemax - region->writemin) return;
 
+	//range in subbuffer
+	int4 ird = iwr;
+	ird.y += region->readmin - region->writemin;
+	int subindex = indexForInt4ForSize(ird, region->size.x, region->size.y, region->size.z);
+
+	//range in the super buffer
+	int4 i = iwr;
+	i.y += region->writemin;
+	
 	//boundary
 	if (i.x == 0 || 
 		i.y == 0 ||
-		i.x == size.x-1 ||
-		i.y == size.y-1)
+		i.x == region->supersize.x-1 ||
+		i.y == region->supersize.y-1)
 	{
-		y[index] = x[index];
+		y[subindex] = x[subindex];
 		return;
 	}
 
 	//PDE
 	const real hSq = .01;
-	y[index] = -(
-		x[index + stepsize.x]
-		+ x[index - stepsize.x]
-		+ x[index + stepsize.y]
-		+ x[index - stepsize.y]
-		- 4. * x[index]
+	y[subindex] = -(
+		x[subindex + region->step.x]
+		+ x[subindex - region->step.x]
+		+ x[subindex + region->step.y]
+		+ x[subindex - region->step.y]
+		- 4. * x[subindex]
 	) / hSq;
-]],
-	event = event,
 }
+
+]],
+	}:concat'\n'
+}
+program:compile()
+
+
+local function chopUpBuffer(buf)
+	local bufs = table()
+	for i,device in ipairs(env.devices) do
+		local region = regions[i]
+		local start = region[0].step.y * region[0].readmin
+		local fin = region[0].step.y * region[0].readmax
+		bufs:insert(buf:subBuffer{
+			start = start,
+			count = fin - start,
+		})
+	end
+	return bufs
+end
+
+local subBuffersForBuffer = table()
+local function getSubBuffers(buf)
+	local sbufs = subBuffersForBuffer[buf]
+	if not sbufs then 
+		sbufs = chopUpBuffer(buf)
+		subBuffersForBuffer[buf] = sbufs 
+	end
+	return sbufs
+end
+
+
+local bInitKernel = program:kernel'bInit'
+local bs = getSubBuffers(b)
+for i,cmd in ipairs(env.cmds) do
+	--bInitKernel.obj:setArgs(regionBufs[i], bs[i])
+	bInitKernel.obj:setArg(0, regionBufs[i])
+	bInitKernel.obj:setArg(1, bs[i])
+	cmd:enqueueNDRangeKernel{
+		kernel = bInitKernel.obj,
+		dim = env.dim,
+		-- kernel domain is based on the write range
+		-- while the buffer is based on the read range
+		globalSize = domains[i].globalSize:ptr(),
+		localSize = domains[i].localSize:ptr(),
+	}
+end
+for i,cmd in ipairs(env.cmds) do
+	cmd:finish()
+end
+
+local event = require 'cl.event'()
+local AKernel = program:kernel'A'
+
+
+local function A(Y,X)
+	-- pick the subbuffers for the buffer ... to chop up the write
+	-- the read will still be a solid buffer (right?)
+	-- or chop up them both?
+	local Xs = getSubBuffers(X)
+	local Ys = getSubBuffers(Y)
+
+	for i,cmd in ipairs(env.cmds) do
+		AKernel.obj:setArgs(regionBufs[i], Ys[i], Xs[i])
+		cmd:enqueueNDRangeKernel{
+			kernel = AKernel.obj,
+			dim = env.dim,
+			globalSize = domains[i].globalSize:ptr(),
+			localSize = domains[i].localSize:ptr(),
+			event = event,
+		}
+	end
+	for i,cmd in ipairs(env.cmds) do
+		cmd:finish()
+	end
+end
 
 local function splot(gpubuf, name)
 	local cpubuf = gpubuf:toCPU()
@@ -73,9 +244,6 @@ local function splot(gpubuf, name)
 end
 
 splot(b, 'b')
-
--- TODO returning 'x' doesn't return an env:buffer object
-local x = env:buffer{name='x'}
 
 for _,solver in ipairs{
 	--'conjgrad',
@@ -97,7 +265,9 @@ for _,solver in ipairs{
 	splot(x, 'x-'..solver)
 end
 
-env.cmds[1]:finish()
+for _,cmd in ipairs(env.cmds) do
+	cmd:finish()
+end
 local start = event:getProfilingInfo'CL_PROFILING_COMMAND_START'
 local fin = event:getProfilingInfo'CL_PROFILING_COMMAND_END'
 print('duration', tonumber(fin - start)..' ns')
